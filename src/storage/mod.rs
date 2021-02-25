09af294533bb78230890fde8d71bc77632df24e7
thomdoherty <thomdoherty@yahoo.com>
2021-02-25 14:05:23 +0000

    Allow particle file to be saved as uuid. Attempt at new tests to count files and implementation

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 
 use crate::{Generation, Weighted, error::Error};
 use crate::error::Result;
+use uuid::Uuid;
 
 trait Storage {
     fn check_active_gen_id(&self) -> Result<u16>;
@@ -52,7 +53,8 @@ impl Storage for FileSystem<'_> {
     }
 
     fn save_particle<P: Serialize>(&self, w: &Weighted<P>) -> Result<String> {
-        let file_path = self.base_path.join("some_file.json");
+        let file_uuid = Uuid::new_v4();
+        let file_path = self.base_path.join(file_uuid.to_string()+".json");
         
         let pretty_json = serde_json::to_string_pretty(w);
         std::fs::write(&file_path, pretty_json?)?;
@@ -61,8 +63,19 @@ impl Storage for FileSystem<'_> {
     }
 
     fn num_particles_available(&self) -> Result<u16> {
-        todo!()
+        //todo!()
+        // // Get current non finished gen?
+        let gen_no = self.check_active_gen_id().to_string();
+        let dir = self.base_path.join(gen_no);
+        let files = std::fs::read_dir(dir).unwrap();
+        let particle_file_names = 
+                 files
+                     .filter(|v| v.path().extension() == Some(OsStr::from_bytes(b"json")))
+               //      .for_each(|f| println!("{:?}",f));
+                     .collect();
+         Result::Ok(particle_file_names.count).unwrap_or(0))
     }
+
     fn retrieve_all_particles<P>(&self) -> Vec<Weighted<P>> {
         todo!()
     }
@@ -153,4 +166,42 @@ mod tests {
         
         assert_eq!(w1, loaded);
     }
+
+    #[test]
+    fn test_no_particle_files() {
+        let full_path = manifest_dir().join("resources/test/fs/empty");
+        let storage = storage(&full_path);
+        assert_eq!(0,storage.num_particles_available().unwrap())
+    }
+
+
+    #[test]
+    fn test_number_particle_files() {
+        let tmp_dir = TmpDir::new("save_particle");
+
+        let p1 = DummyParams::new(1,2.);
+        let p2 = DummyParams::new(2,3.);
+        let p3 = DummyParams::new(3,4.);
+        let p4 = DummyParams::new(4,5.);
+
+        let w1 = Weighted {
+            scored_vec: vec![Scored::new(p1,1.0), Scored::new(p2,2.0)],
+            weight: 3.0,
+        };
+
+        let w2 = Weighted {
+            scored_vec: vec![Scored::new(p3,3.0), Scored::new(p4,4.0)],
+            weight: 2.0,
+        };
+
+        let storage = storage(&tmp_dir.0);
+
+        let saved_1 = storage.save_particle(&w1).unwrap();
+        println!("File 1 was saved to {}", saved_1);
+        let saved_2 = storage.save_particle(&w2).unwrap();
+        println!("File 2 was saved to {}", saved_2);
+        assert_eq!(2,storage.num_particles_available().unwrap())
+    }
+
+
 }
