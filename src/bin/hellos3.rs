5852fa93117cbe7d78d95b2b322fc94149b4f334
thomdoherty <thomdoherty@yahoo.com>
2021-06-15 16:57:39 +0100

    Started changing to test paging request when listing in s3. Not compiling yet.

--- src/bin/hellos3.rs
+++ src/bin/hellos3.rs
@@ -4,32 +4,37 @@ use rusoto_core::Region;
 
 #[tokio::main]
 async fn main() ->  Result<(), Box<dyn std::error::Error>> {
-    // let client = rusoto_sts::StsClient::new(Region::EuWest1);
-    // client.
 
     let s3_client = S3Client::new(Region::EuWest1);
-    let fut = s3_client.list_objects_v2(ListObjectsV2Request{
+    let mut list_obj_req = ListObjectsV2Request{
         bucket: String::from("s3-ranch-007"),
-        prefix: Some("example/gen_002/".to_string()),
+        prefix: Some("test1000PlusS3".to_string()),
         ..Default::default()
-    });
+    };
+    let fut = s3_client.list_objects_v2( list_obj_req);
 
-    let response = fut.await?;
+    let mut results = vec![];
 
-    for thing in response.contents.unwrap() {
-        println!("{:#?}", thing.key.unwrap());
+    loop {
+    let result = fut.await.unwrap();
+
+    // for thing in response.contents.unwrap() {
+    //     println!("{:#?}", thing.key.unwrap());
+    // }
+
+        match result.contents {
+            Some(x) => results.extend(x),
+            None => break,
+        }
+
+        list_obj_req.continuation_token = result.next_continuation_token;
+        if list_obj_req.continuation_token.is_none() {
+            break;
+        }
     }
 
-    
+    println!("Listed {:?} objects.", results);
 
     Result::Ok(())
-    //TODO this will only list up to 1000... what about the rest?
-
-    // s3_client.put_object(List {
-    //     bucket: String::from("s3-ranch-007"),
-    //     key: "@types.json".to_string(),
-    //     body: Some(json::stringify(paths)),
-    //     acl: Some("public-read".to_string()),
-    //     ..Default::default()
-    // }).sync().expect("could not upload");
 }
+
