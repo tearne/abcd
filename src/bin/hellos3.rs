4ebe3b87e005204d9928e2cf29b3a7e706705568
thomdoherty <thomdoherty@yahoo.com>
2021-06-16 10:45:14 +0100

    Test of continuation token working - to grab > 1000 objs from s3

--- src/bin/hellos3.rs
+++ src/bin/hellos3.rs
@@ -4,37 +4,28 @@ use rusoto_core::Region;
 
 #[tokio::main]
 async fn main() ->  Result<(), Box<dyn std::error::Error>> {
+    // let client = rusoto_sts::StsClient::new(Region::EuWest1);
+    // client.
 
     let s3_client = S3Client::new(Region::EuWest1);
     let mut list_obj_req = ListObjectsV2Request{
-        bucket: String::from("s3-ranch-007"),
-        prefix: Some("test1000PlusS3".to_string()),
+        bucket: String::from("s3-ranch-007").to_owned(),
+        prefix: Some("test1000PlusS3".to_string().to_owned()),
         ..Default::default()
     };
-    let fut = s3_client.list_objects_v2( list_obj_req);
-
-    let mut results = vec![];
-
-    loop {
-    let result = fut.await.unwrap();
-
-    // for thing in response.contents.unwrap() {
-    //     println!("{:#?}", thing.key.unwrap());
-    // }
-
-        match result.contents {
-            Some(x) => results.extend(x),
-            None => break,
+    let mut count = 0u32;
+    loop{
+        let result = s3_client.list_objects_v2(list_obj_req.clone());
+        let response = result.await?;
+        for thing in response.contents.unwrap() {
+            count +=1;
+            println!("{:#?}", thing.key.unwrap());
         }
-
-        list_obj_req.continuation_token = result.next_continuation_token;
-        if list_obj_req.continuation_token.is_none() {
+        list_obj_req.continuation_token = response.next_continuation_token.clone();
+        if list_obj_req.continuation_token.is_none(){
             break;
         }
     }
-
-    println!("Listed {:?} objects.", results);
-
+    println!("{:#?}", count);
     Result::Ok(())
 }
-
