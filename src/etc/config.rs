7f98cb2fe4e10639b60a6be8ba66f21c33e392a2
thomdoherty <thomdoherty@yahoo.com>
2021-07-07 10:12:28 +0100

    Worked on unit tests for config

--- src/etc/config.rs
+++ src/etc/config.rs
@@ -30,34 +30,47 @@ impl Config {
     {
         let str = std::fs::read_to_string(config_path.as_ref())
             .unwrap_or_else(|e| panic!("Failed to load config from {:?}: {}", config_path, e));
-        let mut config: Self = toml::from_str(&str).unwrap();
-        config.system_params.absoluteify_root_path(config_path);
+            println!(" str ========> {}", str);
+            let mut config: Self = toml::from_str(&str).unwrap();
         config
     }
 
     pub fn resolve(&mut self){
-        if self.bucket.starts_with('$') {
-            let var_name = self.bucket;
+        //if self.bucket.starts_with('$') { //NOTE: get doesn't like $ in front of var_name
+            let var_name = self.bucket.clone();
+            println!(" var_name ========> {}", var_name);
             //TODO test panics if can't find it
-            self.bucket = envmnt::get(var_name);
-        }
+            self.bucket = envmnt::get_or_panic(var_name);
+       // }
     }
+
 }
 
 #[cfg(test)]
 mod tests {
-    fn error_if_dont_call_resolve_when_using_env_var(){ todo!() }
+    use std::path::PathBuf;
+    use crate::etc::config::Config;
 
-    fn load_from_env_var() {
-        if !envmnt::exists("$ABCDBucket") {
-            envmnt::set("$ABCDBucket", "info");
-        }
-
+    #[test]
+    #[should_panic]
+    fn error_if_dont_call_resolve_when_using_env_var(){ 
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         d.push("resources/test/config.toml");
         let mut config = Config::from_path(d);
+        assert_eq!("s3://my-bucket", config.bucket);
+     }
+
+     #[test]
+    fn load_from_env_var() {
+        // if !envmnt::exists("$ABCDBucket") {
+        //     envmnt::set("$ABCDBucket", "info"); //Do we really want to set this in code?
+        // }
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("resources/test/config.toml");
+        //println!(" path to toml file ========> {}", d.clone().into_os_string().into_string().unwrap());
+        let mut config = Config::from_path(d);
         config.resolve();  //Badly named function which resolves env vars
 
-        assert_eq!("s3://my-bucket", config.bucket);
+       assert_eq!("s3://my-bucket", config.bucket);
     }
 }
