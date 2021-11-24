7559ec10877be653ba42e47f129500f452e33f5a
thomdoherty <thomdoherty@yahoo.com>
2021-11-24 09:59:45 +0000

    Removed older config

--- src/lib.rs
+++ src/lib.rs
@@ -48,10 +48,10 @@ struct Generation<P> {
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-pub struct s3_params {
+pub struct S3Params {
     input_data_root: PathBuf, //TODO can we make this a Path? lifetime seems to clash
 }
-impl s3_params {
+impl S3Params {
     pub fn absoluteify_root_path(&mut self, config_path: impl AsRef<Path>) {
         if !self.input_data_root.starts_with("/") {
             self.input_data_root = config_path
@@ -60,22 +60,5 @@ impl s3_params {
                 .unwrap()
                 .join(self.input_data_root.as_path())
         };
-    }
-}
-
-#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-pub struct Config {
-    pub s3_params: s3_params,
-}
-impl Config {
-    pub fn from_path<P>(config_path: P) -> Self
-    where
-        P: AsRef<Path> + Debug,
-    {
-        let str = std::fs::read_to_string(config_path.as_ref())
-            .unwrap_or_else(|e| panic!("Failed to load config from {:?}: {}", config_path, e));
-        let mut config: Self = toml::from_str(&str).unwrap();
-        config.s3_params.absoluteify_root_path(config_path);
-        config
     }
 }
