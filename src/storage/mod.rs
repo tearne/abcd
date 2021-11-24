a9e6ccb2a752a94dada100b206b84f7f3e33d7f7
thomdoherty <thomdoherty@yahoo.com>
2021-11-24 10:00:42 +0000

    Removed lifetime

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -10,16 +10,21 @@ use std::fmt::Debug;
 
 trait Storage {
     fn check_active_gen(&self) -> Result<u16>;
-    fn retrieve_previous_gen<'de, P>(&self) -> Result<Generation<P>>
+
+    fn retrieve_previous_gen<P>(&self) -> Result<Generation<P>>
     where
         P: DeserializeOwned + Debug;
+
     fn save_particle<P>(&self, w: &Particle<P>) -> Result<String>
     where
         P: Serialize + Debug;
+
     fn num_particles_available(&self) -> Result<u16>;
+
     fn retrieve_all_particles<P>(&self) -> Result<Vec<Particle<P>>>
     where
         P: DeserializeOwned + Debug;
+
     fn save_new_gen<P>(&self, g: Generation<P>) -> Result<()>
     where
         P: Serialize + Debug;
