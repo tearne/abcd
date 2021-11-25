2391136fbe3c350938ccca5ea4ed0237622ca178
thomdoherty <thomdoherty@yahoo.com>
2021-11-25 16:41:37 +0000

    Add high level abc config in comments to inform design

--- src/etc/config.rs
+++ src/etc/config.rs
@@ -1,22 +1,32 @@
 use std::fmt::Debug;
 use std::path::{Path, PathBuf};
 
-// #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-// pub struct SystemParams {
-//     input_data_root: PathBuf //TODO can we make this a Path? lifetime seems to clash
-// }
-// impl SystemParams {
-//     // pub fn absoluteify_root_path(&mut self, config_path: impl AsRef<Path>) {
-//     //     if !self.input_data_root.starts_with("/") {
-//     //         self.input_data_root = config_path
-//     //             .as_ref()
-//     //             .parent()
-//     //             .unwrap()
-//     //             .join(self.input_data_root.as_path())
-//     //     };
-//     // }
-
-// }
+// abc {
+//     job {
+//         replicates = 4
+//         particles = 1000
+//         generations = 50
+//     }	
+//     algorithm {
+//         particle-retries = 100  <--- will we still want this?
+//         particle-chunk-size = 1 <--- probably don't need it
+//         tolerance-descent-percentile = 0.5
+//         fewest-accepted-local-particles = 0 <--- gone
+//     }
+//     cluster {
+            //Add the storage in here
+//         system-name: ${CLUSTER_NAME}
+//         max-particle-memory = 1000000
+//         terminate-at-target-generation = false <--- perhaps keep?
+//         futures-timeout = 90 days
+//          mixing {
+//              rate = 1 minutes
+//              num-particles = 500
+//             response-threshold = 5 seconds
+//         }
+//         size-reporting = 1 hour
+//     }
+//  }
 
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
@@ -40,6 +50,8 @@ impl Storage {
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Config {
     pub storage: Storage,
+    pub job: Job,
+    pub algorithm: Algorithm,
 }
 impl Config {
     pub fn from_path<P>(config_path: P) -> Self
@@ -49,8 +61,8 @@ impl Config {
         let str = std::fs::read_to_string(config_path.as_ref())
             .unwrap_or_else(|e| panic!("Failed to load config from {:?}: {}", config_path, e));
         
-        log::info!("Loading config: {}", str);
         let config: Config = toml::from_str(&str).unwrap();
+        log::info!("Loading config: {:#?}", config);
         config
     }
 }
@@ -62,9 +74,7 @@ mod tests {
 
      #[test]
     fn load_from_env_var() {
-        if !envmnt::exists("ABCDBucket") {
-            envmnt::set("ABCDBucket", "s3://my-bucket");
-        }
+        envmnt::set("ABCDBucket", "s3://my-bucket");
         
         //TODO put in helper
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
