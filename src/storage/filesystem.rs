13cfa82fb9fd127869d3d3d68a5f2475d9e98515
thomdoherty <thomdoherty@yahoo.com>
2022-02-15 13:02:26 +0000

    Looked at checking gen for gen 0 used for prior

--- src/storage/filesystem.rs
+++ src/storage/filesystem.rs
@@ -13,6 +13,8 @@ use crate::{
 };
 use uuid::Uuid;
 
+use crate::storage::filesystem::ABCDError::NoGenZeroDirExists;
+
 use super::Storage;
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
@@ -54,7 +56,7 @@ impl Storage for FileSystem {
     fn check_active_gen(&self) -> ABCDResult<u16> {
         let re = Regex::new(r#"^gen_(?P<gid>\d*)$"#)?;
 
-        let max: Option<u16> = std::fs::read_dir(&self.base_path)?
+        let gen_dirs = std::fs::read_dir(&self.base_path)?
             .filter_map(|read_dir| {
                 let path = read_dir.as_ref().ok()?.path();
                 if path.is_dir() {
@@ -71,13 +73,21 @@ impl Storage for FileSystem {
                 } else {
                     None
                 }
-            })
-            .max();
+            });
+            
+            let max_with_gen_0_check = if gen_dirs.collect::<Vec<u16>>().contains(&0) {
+                &gen_dirs.max()
+            } else {
+                //return Err(ABCDError::NoGenZeroDirExists("No Gen Zero Directory Exists".to_string())); //Should throw helpful exception here to point out no gen 0 available?
+                None 
+            };
+        
+            
         //TODO change this to handle first gen (gen 0) - where no directory exists
         // How do we tell the difference between not having started yet, vs having given the wrong storage location?
         // Do we need a marker?  Like some kind of `gen_0` thing?
         //TODO need to make sure that storage refuses to work if gen_000 is missing?  Add new tests?
-        max.ok_or_else(|| ABCDError::Other("Failed to find max gen.".into()))
+        max_with_gen_0_check.ok_or_else(|| ABCDError::Other("Failed to find max gen.".into()))
     }
 
     // fn retrieve_previous_gen<'de, P>(&self) -> Result<Generation<P>> where P: Deserialize<'de>;
@@ -306,8 +316,21 @@ mod tests {
     }
 
     #[test]
-    fn test_check_active_gen_exception() {
-        let full_path = manifest_dir().join("resources/test/fs/empty/");
+    // fn test_check_active_gen_exception_noGenZeroExists() {
+    //     let full_path = manifest_dir().join("resources/test/fs/empty/");
+    //     let storage = FileSystem::new(full_path);
+    //     let result = storage.check_active_gen();
+    //     let expected_message = "No Gen Zero Directory Exists";
+
+    //     match result {
+    //         Ok(_) => panic!("Expected error"),
+    //         Err(ABCDError::NoGenZeroDirExists(msg)) if msg == expected_message => (),
+    //         Err(e) => panic!("Wrong error, got: {}", e),
+    //     };
+    // }
+
+    fn test_check_active_gen_exception_GenZeroDoesExist() { //Actually turn this into test for active Gen = 0?
+        let full_path = manifest_dir().join("resources/test/fs/empty_WithGen0/");
         let storage = FileSystem::new(full_path);
         let result = storage.check_active_gen();
         let expected_message = "Failed to find max gen.";
