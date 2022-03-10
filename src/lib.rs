610615d188af1f706e971916153a68f78b7683a2
Tearne <oliver.tearne@apha.gov.uk>
2022-03-10 21:41:38 +0000

    Fix GenerationStuff

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,8 @@ mod etc;
 mod storage;
 mod types;
 
-use std::{thread::Thread, convert::TryInto};
-
 use error::{ABCDError, ABCDResult};
 use etc::config::Config;
-use rand::distributions::WeightedIndex;
 use rand::prelude::*;
 use storage::Storage;
 pub use types::{ Model, Generation, Particle};
@@ -33,60 +30,47 @@ pub fn run<M: Model, S: Storage>(
     };
 
     loop {
-        // let gen = storage.load_previous_gen()?;
-        // let number = do_gen(
-        //     &storage,
-        //     &model,
-        //     &config,
-        //     random,
-        //     ActualGeneration::new(gen,config),
-        // )?;
-        // if number == config.job.num_generations && config.job.terminate_at_target_gen {
-        //     break;
-        // }
+        let gen = storage.load_previous_gen()?;
+        let number = do_gen(
+            &storage,
+            &model,
+            &config,
+            random,
+            EmpiricalGeneration{gen, config: config.clone()},
+        )?;
+        if number == config.job.num_generations && config.job.terminate_at_target_gen {
+            break;
+        }
     }
 
     Ok(())
 }
 
-trait GenerationStuff {
-    fn propose_me_a_parmeter_set<M: Model>(&self, model: &M, random: &ThreadRng) -> M::Parameters;
-    fn calculate_me_a_tolerance(&self) -> f64;
-    fn weigh_me_a_particle<M: Model>(&self,scores: &Vec<f64>, tolerance: f64, model: &M) -> Particle<M::Parameters>;
+trait GenerationOps<P> {
+    fn propose<M: Model<Parameters = P>>(&self, model: &M, random: &ThreadRng) -> P;
+    fn calculate_tolerance(&self) -> f64;
+    fn weigh<M: Model<Parameters = P>>(&self, scores: &[f64], tolerance: f64, model: &M) -> Particle<P>;
 
-    fn calculate_fhat(scores: &Vec<f64>, tolerance: f64) -> f64 {
+    fn calculate_fhat(scores: &[f64], tolerance: f64) -> f64 {
         // (B5b) Calculate f^hat by calc'ing proportion less than tolerance
         let number_reps = cast::f64(scores.len());
-        let number_reps_less_than_tolerance = scores.iter()
-            .filter_map(|score| 
-                if *score < tolerance { Some(score) }
-                else { None }
-            )
-            .collect::<Vec<_>>()
-            .len();
-        let fhat = cast::f64(number_reps_less_than_tolerance)/number_reps;
-        fhat
+        let number_reps_less_than_tolerance = scores
+            .iter()
+            .filter(|score| **score < tolerance)
+            .count();
+        cast::f64(number_reps_less_than_tolerance)/number_reps
+    }
 }
-}
-struct ActualGeneration<P>{
+struct EmpiricalGeneration<P>{
     gen: Generation<P>,
     config: Config
 }
-impl<P> ActualGeneration<P> {
-    fn new(gen: Generation<P>, config: Config) -> Self {
-        Self { gen, config }
-    }
-}
-impl<Params> GenerationStuff for ActualGeneration<Params> {
-    //TODO
-    // https://stackoverflow.com/questions/40553023/check-for-equal-associated-types-in-where-clause
-    fn propose_me_a_parmeter_set<M>(&self, model: &M, random: &ThreadRng) -> Params 
-    where M: Model<Params = <M as Model>::Parameters>
-    {
+impl<P> GenerationOps<P> for EmpiricalGeneration<P> {
+    fn propose<M: Model<Parameters = P>>(&self, model: &M, random: &ThreadRng) -> P {
         todo!()
     }
 
-    fn calculate_me_a_tolerance(&self) -> f64 {
+    fn calculate_tolerance(&self) -> f64 {
         // Get distribution of scores from last generation then reduce by tolerance descent rate (configured) - crate exists for percentile => 
         let score_distribution: Vec<f64> = self.gen
             .pop
@@ -105,70 +89,40 @@ impl<Params> GenerationStuff for ActualGeneration<Params> {
         new_tolerance
     }
 
-    fn weigh_me_a_particle(&self, scores: &Vec<f64>, tolerance: f64, model: &M) -> Particle<<M as Model>::Parameters> {
-    // (B6) Calculate not_normalised_weight for each particle from its f^hat (f^hat(p) * prior(p)) / denom)
-        let fhat = GenerationStuff::calculate_fhat(scores,tolerance);
+    fn weigh<M: Model<Parameters = P>>(&self, scores: &[f64], tolerance: f64, model: &M) -> Particle<P> {
+        // (B6) Calculate not_normalised_weight for each particle from its f^hat (f^hat(p) * prior(p)) / denom)
+        let fhat = Self::calculate_fhat(scores, tolerance);
         todo!()
     }
-
-
 }
 struct PriorGeneration{}
-impl GenerationStuff for PriorGeneration {
-    fn propose_me_a_parmeter_set<M: Model> (&self, model: &M, random: &ThreadRng) -> <M as Model>::Parameters {
+impl<P> GenerationOps<P> for PriorGeneration {
+    fn propose<M: Model<Parameters = P>> (&self, model: &M, random: &ThreadRng) -> P {
         todo!()
     }
 
-    fn calculate_me_a_tolerance(&self) -> f64 {
+    fn calculate_tolerance(&self) -> f64 {
         f64::MAX
     }
 
-    fn weigh_me_a_particle<M: Model>(&self,  model: &M) -> Particle<<M as Model>::Parameters> {
-            // Get distribution of scores from last generation then reduce by tolerance descent rate (configured) - crate exists for percentile => 
-    // (B5b) Calculate f^hat by calc'ing proportion less than tolerance
-    // (B6) Calculate not_normalised_weight for each particle from its f^hat (f^hat(p) * prior(p)) / denom)
+    fn weigh<M: Model<Parameters = P>>(&self,  scores: &[f64], tolerance: f64, model: &M) -> Particle<P> {
+        // Get distribution of scores from last generation then reduce by tolerance descent rate (configured) - crate exists for percentile => 
+        // (B5b) Calculate f^hat by calc'ing proportion less than tolerance
+        // (B6) Calculate not_normalised_weight for each particle from its f^hat (f^hat(p) * prior(p)) / denom)
         todo!()
     }
     
 }
-
-// struct PriorProposer { //TODO rename to reflect fact that it does two things (a) proposing, (b) weighing
-
-// }
-// impl PriorProposer {
-//     pub fn new() -> Self {
-//         PriorProposer {}
-//     }
-// }
-// impl<M: Model> Proposer<M> for PriorProposer {
-//     fn next(&self, model: &M, random: &mut Random) -> <M as Model>::Parameters {
-//         model.prior_sample(random)
-//     }
-// }
-
-// struct PreviousGenerationProposer<P> {
-//     generation: Generation<P>,
-// }
-// impl<P> PreviousGenerationProposer<P> {
-//     pub fn new(generation: Generation<P>) -> Self {
-//         PreviousGenerationProposer { generation }
-//     }
-// }
-// impl<M: Model> Proposer<M> for PreviousGenerationProposer<M::Parameters> {
-//     fn next(&self, model: &M, random: &mut Random) -> <M as Model>::Parameters {
-//         sample_and_perturb_with_support(&self.generation, model, random)
-//     }
-// }
 
 fn do_gen<M: Model, S: Storage>(
     storage: &S,
     model: &M,
     config: &Config,
     random: &mut Random,
-    gen_stuff: impl GenerationStuff,
+    gen_stuff: impl GenerationOps<M::Parameters>,
 ) -> ABCDResult<u16> {
     let prev_gen_number = storage.previous_gen_number()?;
-    let tolerance = gen_stuff.calculate_me_a_tolerance();
+    let tolerance = gen_stuff.calculate_tolerance();
     loop {
         //Particle loop
 
@@ -176,7 +130,7 @@ fn do_gen<M: Model, S: Storage>(
         // TODO loop could go on forever?  Use some kind of timeout, or issue warning?
         // (B3) sample a (fitting) parameter set from gen (perturb based on weights and kernel if sampling from generation)
         // (B4) Check if prior probability is zero - if so sample again
-        let parameters: <M as Model>::Parameters = gen_stuff.propose_me_a_parmeter_set(model, random);
+        let parameters: <M as Model>::Parameters = gen_stuff.propose(model, random);
 
         let scores: ABCDResult<Vec<f64>> = (0..config.job.num_replicates)
             .map(|rep_idx| {
@@ -194,13 +148,7 @@ fn do_gen<M: Model, S: Storage>(
         // We now have a collection of scores for the particle
         // (B5b) Calculate f^hat by calc'ing proportion less than tolerance
         // (B6) Calculate not_normalised_weight for each particle from its f^hat (f^hat(p) * prior(p)) / denom)
-        let particle: Particle<M::Parameters> = todo!();//gen_stuff.weigh_me_a_particle(scores, model, tolerance);
-        // let particle = algorithm::weigh_particle(scores, f64::MAX, model, prev_gen_number);
-        // let particle = Particle{
-        //     parameters,
-        //     scores,
-        //     weight,
-        // };
+        let particle: Particle<M::Parameters> = gen_stuff.weigh(&scores, tolerance, model);
 
         // Save the non_normalised particle to storage
         storage.save_particle(&particle)?; 
@@ -220,7 +168,7 @@ fn do_gen<M: Model, S: Storage>(
             // let normalised = algorithm::normalise(particles);
             // let new_generation = Generation::new(normalised, prev_gen_number + 1, tolerance, acceptance);
 
-            // // Save generation to storage
+            // Save generation to storage
             // storage.save_new_gen(&new_generation);
 
             // return Ok(new_generation.number);
@@ -231,7 +179,7 @@ fn do_gen<M: Model, S: Storage>(
 
 
 //TODO put this in the previous gen proposer
-fn sample_and_perturb_with_support<M>(
+fn sample_and_perturb_with_support<M> (
     gen: &Generation<M::Parameters>,
     model: &M,
     random: &mut Random,
