9280b1959e633f22400507b12c4d408d15ad845c
Tearne <oliver.tearne@apha.gov.uk>
2022-08-13 21:41:34 +0100

    put back config, printf new lines

--- resources/examples/run_unfair_coin.sh
+++ resources/examples/run_unfair_coin.sh
@@ -12,14 +12,14 @@ done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 cd $DIR
 
-printf "Initialising storage prefix..."
+printf "Initialising storage prefix...\n"
 aws s3 sync empty_prefix s3://${TEST_BUCKET}/${TEST_PREFIX}/ --delete --acl bucket-owner-full-control
 
-printf "Starting application..."
+printf "Starting application...\n"
 sleep 1
 export RUST_LOG=error,abcd=info,unfair_coin=info 
 cargo run --release --example unfair_coin
 
-printf "Downloaing the completed generations..."
+printf "Downloaing the completed generations...\n"
 sleep 1
 aws s3 sync s3://${TEST_BUCKET}/${TEST_PREFIX}/completed ../../out
