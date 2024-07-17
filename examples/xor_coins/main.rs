dcc4ede7fd52a54ea8e05423c5f2acfe66c01355
Tearne <oliver.tearne@apha.gov.uk>
2024-07-17 14:06:52 +0100

    Prob gonna need heap allocation

--- examples/xor_coins/main.rs
+++ examples/xor_coins/main.rs
@@ -1,10 +1,11 @@
-use abcd::{config::Config, Model, ABCD};
+use abcd::{config::Config, error::VectorConversionError, kernel::olcm::{OLCMKernel, OLCMKernelBuilder}, types::Vector, wrapper::GenWrapper, Model, ABCD};
 use color_eyre::eyre;
+use nalgebra::{DVector, SMatrix, Vector2};
 use path_absolutize::Absolutize;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use statrs::distribution::Normal;
-use std::{ops::Range, path::Path};
+use std::{error::Error, ops::Range, path::Path};
 use tokio::runtime::Runtime;
 
 /// We have two coins, A & B, neither of which need be fair.
@@ -22,6 +23,27 @@ use tokio::runtime::Runtime;
 struct MyParameters {
     heads_a: f64,
     heads_b: f64,
+}
+
+impl Vector<2> for MyParameters {
+    fn to_column_vector(&self) -> SMatrix<f64, 2, 1> {
+        Vector2::new(self.heads_a, self.heads_b)
+    }
+
+    fn from_column_vector(v: DVector<f64>) -> Result<Self, abcd::error::VectorConversionError> {
+        let values = v.iter().cloned().collect::<Vec<f64>>();
+            if values.len() != 2 {
+                return Err(VectorConversionError(format!(
+                    "Wrong number of arguments.  Expected 2, got {}",
+                    values.len()
+                )));
+            } else {
+                Ok(MyParameters {
+                    heads_a: values[0],
+                    heads_b: values[1],
+                })
+            }
+    }
 }
 
 #[derive(Debug)]
@@ -94,26 +116,35 @@ impl MyModel {
 
 impl Model for MyModel {
     type Parameters = MyParameters;
-    type K: 
+    type K = OLCMKernel<2, MyParameters>; 
+    type Kb = OLCMKernelBuilder<2, MyParameters>;
+
     fn prior_sample(&self, rng: &mut impl Rng) -> Self::Parameters {
         let heads: f64 = self.prior.sample(rng);
-        MyParameters { heads }
+        MyParameters { 
+            heads_a : self.prior.sample(rng),
+            heads_b : self.prior.sample(rng),
+        }
     }
 
     fn prior_density(&self, p: &Self::Parameters) -> f64 {
         let density: f64 = { self.prior.density(p.heads) };
         density
     }
-
-    fn perturb(&self, _p: &Self::Parameters, rng: &mut impl Rng) -> Self::Parameters {
-        let heads: f64 = _p.heads + self.kernel.sample(rng);
-        MyParameters { heads }
+    
+    fn build_kernel_builder_for_generation(&self, prev_gen: &GenWrapper<Self::Parameters>) -> Result<&Self::Kb, Box<dyn Error>> {
+        OLCMKernelBuilder::new(prev_gen.pa)
     }
 
-    fn pert_density(&self, _from: &Self::Parameters, _to: &Self::Parameters) -> f64 {
-        let pert_density: f64 = { self.kernel.density(_from.heads - _to.heads) };
-        pert_density
-    }
+    // fn perturb(&self, _p: &Self::Parameters, rng: &mut impl Rng) -> Self::Parameters {
+    //     let heads: f64 = _p.heads + self.kernel.sample(rng);
+    //     MyParameters { heads }
+    // }
+
+    // fn pert_density(&self, _from: &Self::Parameters, _to: &Self::Parameters) -> f64 {
+    //     let pert_density: f64 = { self.kernel.density(_from.heads - _to.heads) };
+    //     pert_density
+    // }
 
     fn score(&self, p: &Self::Parameters) -> Result<f64, Self::E> {
         let mut random = rand::thread_rng();
